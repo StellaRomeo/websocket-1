aed90a7a8831c2484a12b17dfe65c3e0303b784d
Stella Romeo <stellaromeo21@gmail.com>
2023-04-23 16:03:19 +0200

    Initial commit

---
+++ src/main/java/co/develhope/websocket1/controller/NotificationController.java
@@ -0,0 +1,23 @@
+package co.develhope.websocket1.controller;
+
+import co.develhope.websocket1.entities.MessageDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @PostMapping("/broadcast")
+    public ResponseEntity sendBroadcast(@RequestBody MessageDTO messageDTO){
+        simpMessagingTemplate.convertAndSend("/topic/broadcast", messageDTO);
+        return ResponseEntity.accepted().body("Message " + messageDTO.getMessage() + " send to /topic/brodcast");
+
+    }
+}
